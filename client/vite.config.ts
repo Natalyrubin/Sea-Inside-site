075781023624656cc8f777fe591847fa11ef4ed2
Natalyrubin <natirubrub@gmail.com>
2024-11-22 16:33:09 +0200

    change

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'client',  // אם תיקיית ה-root היא client
+  build: {
+    rollupOptions: {
+      input: 'client/index.html',  // הגדרת נתיב לקובץ index.html
+    },
+  },
 })
+
+
