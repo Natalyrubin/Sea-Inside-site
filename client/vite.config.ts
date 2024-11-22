aaf1078f77bc7dea3debf91382f6219c73b157e0
Natalyrubin <natirubrub@gmail.com>
2024-11-22 16:54:37 +0200

    up

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'client',  // הוספת הגדרה זו מציינת ש-Vite ימצא את index.html בתיקיית ה-client
-  build: {
-    outDir: '../dist',  // אם תרצה לשמור את הקבצים בתיקיית dist בשורש הפרויקט
-  },
 })
