d3c9a25b63d065cd335606496d751c268d5b3ae4
Natalyrubin <natirubrub@gmail.com>
2024-12-28 22:01:17 +0200

    db

--- server/config/db.js
+++ server/config/db.js
@@ -4,27 +4,29 @@ require('dotenv').config();
 const mode = process.env.NODE_ENV || 'development';
 
 let uri;
-if (mode === 'prod') {
+if (mode === 'production') {
     uri = process.env.MONGODB_URI_PROD;
-    console.log('Running in production mode');
+    console.log('[i] Running in production mode');
 } else if (mode === 'atlas') {
     uri = process.env.MONGODB_URI_ATLAS;
-    console.log('Running in Atlas mode');
+    console.log('[i] Running in Atlas mode');
 } else {
     uri = process.env.MONGODB_URI_DEV;
-    console.log('Running in development mode');
+    console.log('[i] Running in development mode');
 }
 
-console.log('Environment Mode:', mode); // בדקי איזה מצב מוגדר
-console.log('MongoDB URI:', uri);       // הדפיסי את ה-URI שנבחר
-
+console.log('[i] Environment Mode:', mode); // בדיקת המצב המוגדר
+console.log('[i] MongoDB URI:', uri);       // הדפסת ה-URI שנבחר
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(uri);
-        console.log('Successfully connected to MongoDB');
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('[v] Successfully connected to MongoDB');
     } catch (err) {
-        console.log('Error connecting to MongoDB:', err.message);
+        console.error('[x] Error connecting to MongoDB:', err.message);
         process.exit(1);
     }
 };
