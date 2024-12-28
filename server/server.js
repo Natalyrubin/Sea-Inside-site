bc7ddfc5563835ac567bedc351a2d07092d52592
Natalyrubin <natirubrub@gmail.com>
2024-12-28 18:16:13 +0200

    Custom CORS Middleware

--- server/server.js
+++ server/server.js
@@ -13,6 +13,25 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+
+
+// Custom CORS Middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://www.sea-inside.co.il"); // החלף בדומיין המותר
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // אפשר שיטות ספציפיות
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // אפשר כותרות
+    if (req.method === "OPTIONS") {
+        return res.status(200).end(); // תגובה לבקשת OPTIONS
+    }
+    next();
+});
+
+
+
+
+
+
+
 // Routes
 app.use('/api/leads', require('./routes/leadsRoutes'));
 
